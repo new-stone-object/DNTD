5b40b5f1f8cf23ae1533d8a1b3b09dc7a0c4d614
new-stone-object <66228353+new-stone-object@users.noreply.github.com>
2021-02-14 11:24:30 +0800

    update code firstly

---
+++ base_model/ResNet.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+from torch.utils import model_zoo
+import torch.nn.functional as F
+
+class Bottleneck(nn.Module):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, dilation=1):
+        super(Bottleneck, self).__init__()
+        self.conv1      = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1        = nn.BatchNorm2d(planes)
+        self.conv2      = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=(3*dilation-1)//2, bias=False, dilation=dilation)
+        self.bn2        = nn.BatchNorm2d(planes)
+        self.conv3      = nn.Conv2d(planes, planes*4, kernel_size=1, bias=False)
+        self.bn3        = nn.BatchNorm2d(planes*4)
+        self.downsample = downsample
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
+        out = F.relu(self.bn2(self.conv2(out)), inplace=True)
+        out = self.bn3(self.conv3(out))
+        if self.downsample is not None:
+            x = self.downsample(x)
+        return F.relu(out+x, inplace=True)
+
+
+class ResNet(nn.Module):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        self.inplanes = 64
+        self.conv1    = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1      = nn.BatchNorm2d(64)
+        self.layer1   = self.make_layer( 64, 3, stride=1, dilation=1)
+        self.layer2   = self.make_layer(128, 4, stride=2, dilation=1)
+        self.layer3   = self.make_layer(256, 6, stride=2, dilation=1)
+        self.layer4   = self.make_layer(512, 3, stride=2, dilation=1)
+
+    def make_layer(self, planes, blocks, stride, dilation):
+        downsample    = nn.Sequential(nn.Conv2d(self.inplanes, planes*4, kernel_size=1, stride=stride, bias=False), nn.BatchNorm2d(planes*4))
+        layers        = [Bottleneck(self.inplanes, planes, stride, downsample, dilation=dilation)]
+        self.inplanes = planes*4
+        for _ in range(1, blocks):
+            layers.append(Bottleneck(self.inplanes, planes, dilation=dilation))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out1 = F.relu(self.bn1(self.conv1(x)), inplace=True)
+        out1 = F.max_pool2d(out1, kernel_size=3, stride=2, padding=1)
+        out2 = self.layer1(out1)
+        out3 = self.layer2(out2)
+        out4 = self.layer3(out3)
+        out5 = self.layer4(out4)
+        return out5, out4, out3, out2, out1
+
+def resnet50(pretrained=False):
+    return ResNet()
+	
+	
